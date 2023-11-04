a5ef50a34fc3f8cb5dc2ca2e2dc08c8fd8d7f768
ramonabroberg <>
2023-11-04 15:55:37 +0000

    Make it possible for users to add tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // sidenav initialization
-  let sidenav = document.querySelectorAll(".sidenav");
-  M.Sidenav.init(sidenav);
+    // sidenav initialization
+    let sidenav = document.querySelectorAll(".sidenav");
+    M.Sidenav.init(sidenav);
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
 });
